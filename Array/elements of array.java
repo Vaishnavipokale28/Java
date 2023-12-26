2706f8714046589243a4462419c6ac2ebbc1d6ef
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-12-27 04:27:20 +0530

    Elements of an Array
    Program to print the elements of an array

---
+++ Array/elements of array.java
@@ -0,0 +1,10 @@
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println("Elements of the array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
