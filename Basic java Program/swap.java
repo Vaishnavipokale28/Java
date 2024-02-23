47c727109cbc0e99157fda13fa97f6ee5901b7e0
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-02-23 13:55:43 +0530

    Swap 2 numbers
    Swap two numbers without using third variable

---
+++ Basic java Program/swap.java
@@ -0,0 +1,15 @@
+class Program5 {
+	public static void main(String args[])
+{
+	int a=10;
+	int b=20;
+		System.out.println("a=" +a + "b=" +b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a=" +a + "b=" +b);
+
+}
+
+}
+ 
