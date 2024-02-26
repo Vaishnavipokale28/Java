e2e15714021907eef57c77038aca1d1dd623cd21
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-02-26 14:03:22 +0530

    Java Pattern Program7

---
+++ pattern/Pattern7.java
@@ -0,0 +1,16 @@
+class Pattern7
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=n; i>=1; i--)
+		{
+			for(int j=1; j<=n; j++)
+			{
+				System.out.print((char)(i+96));
+			}
+			System.out.println();
+		}
+			
+	}
+}
