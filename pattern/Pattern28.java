24f0b190575e1483bc4165e9a9c81844cfe09e78
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-02-28 16:09:48 +0530

    Java Counter program

---
+++ pattern/Pattern28.java
@@ -0,0 +1,21 @@
+class Pattern28
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int count=15;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(count +"\t");
+				count--;
+			}
+			System.out.println();
+		}
+	}
+}
+    
+   
+
+
