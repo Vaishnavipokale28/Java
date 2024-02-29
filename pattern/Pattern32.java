182665f3d7a2e5b514606111a7af88539587001c
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-02-29 14:18:04 +0530

    Java counter program 32

---
+++ pattern/Pattern32.java
@@ -0,0 +1,20 @@
+class Pattern32
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int count=1;
+		for(int i=n;i>=1;i--)
+		{
+			int c=count;
+			for(int j=n;j>=i;j--)
+			{
+				System.out.print(c+"\t");
+				c+=j-1;
+			}
+			System.out.println();
+			count++;
+		}
+	}
+
+}
