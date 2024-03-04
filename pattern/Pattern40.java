440b10bea0e4ca2557a25c93413d45c7894b3343
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-03-04 13:27:13 +0530

    Java counter program 40

--- pattern/Pattern40.java
+++ pattern/Pattern40.java
@@ -1,23 +1,23 @@
-class  Pattern40
+class Pattern40
 {
 	public static void main(String args[])
 	{
 		int n=5;
-		int count=n;
-		for(int i=n;i>=1;i--)
+		int count=1;
+		for(int i=1;i<=n;i++)
 		{
 			int c=count;
-			for(int j=i;j<n;j++)
+			for(int j=i;j>1;j--)
 			{
 				System.out.print("\t");
 			}
-			for(int j=i;j>=1;j--)
-			{
+			for(int j=n;j>=i;j--)
+			{	
 				System.out.print(c+"\t");
-				c--;
+				c+=j;
 			}
 			System.out.println();
-			count-=i-1;
+			count++;
 		}
 	}
 }
