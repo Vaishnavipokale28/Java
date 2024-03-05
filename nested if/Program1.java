e65d5abaad95eba487a53a99c3baec183ce5aa46
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-03-05 15:40:04 +0530

    Nested if program1
    write a program to find greater number among of 4

---
+++ nested if/Program1.java
@@ -0,0 +1,15 @@
+class Program1
+{
+	public static void main(String args[])
+	{
+		double p=65.45;
+		if(p>=70)
+			System.out.print("distinct");
+		else if(p>=60)
+			System.out.print("first class");
+		else if(p>=50)
+			System.out.print(" second class");
+		else
+			System.out.print(" failed");	
+	}
+}
