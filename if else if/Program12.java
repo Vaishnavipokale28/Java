a1ef17379c412bc3d774b9c94ce4d478e3f48153
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-03-06 16:04:38 +0530

    Java if else if program 12

---
+++ if else if/Program12.java
@@ -0,0 +1,18 @@
+class Program12
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(j>i)
+					System.out.print(j);
+				else
+					System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
