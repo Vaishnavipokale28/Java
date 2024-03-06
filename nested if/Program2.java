e6984bfff9fbcedc70223f9b23702e798bbc5a4e
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-03-06 23:37:22 +0530

    Java nested if program2

---
+++ nested if/Program2.java
@@ -0,0 +1,106 @@
+class Program2
+{
+	public static void main(String args[])
+	{
+		int a=3000, b=500, c=600, d=400, e=4000, f=2000;
+		if(a>b)
+			if(a>c)
+				if(a>d)
+					if(a>e)
+						if(a>f)
+							System.out.println("a is greater");
+						else
+							System.out.println(" f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else
+							System.out.println("f is greater");
+				else
+					if(d>e)
+						if(d>f)
+							System.out.println(" d is greater");
+						else
+							System.out.println("f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else
+							System.out.println("f is greater");
+			else
+				if(c>d)
+					if(c>e)
+						if(c>f)
+							System.out.println("c is greater");
+						else
+							System.out.println("f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else
+							System.out.println("f is greater");
+				else
+					if(d>e)
+						if(d>f)
+							System.out.println("d is greater");
+						else
+							System.out.println("f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else
+							System.out.println("f is greater");
+		else
+			if(b>c)
+				if(b>d)
+					if(b>e)
+						if(b>f)
+							System.out.println("b is greater");
+						else
+							System.out.println("f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else
+							System.out.println("f is greater");
+				else
+					if(d>e)
+						if(d>f)
+							System.out.println("d is greater");
+						else
+							System.out.println("f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else
+							System.out.println("f is greater");
+			else
+				if(c>d)
+					if(c>e)
+						if(c>f)
+							System.out.println("c is greater");
+						else
+							System.out.println("f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else
+							System.out.println("f is greater");
+				else
+					if(d>e)
+						if(d>f)
+							System.out.println("d is greater ");
+						else
+							System.out.println("f is greater");
+					else
+						if(e>f)
+							System.out.println("e is greater");
+						else 
+							System.out.println("f is greater");
+
+					
+					
+						
+	}
+
+}
