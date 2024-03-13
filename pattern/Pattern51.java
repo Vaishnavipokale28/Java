a4b0e8acd165180feb0b786990e9ab55824d00bc
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-03-13 13:42:03 +0530

    Java Pattern program51

---
+++ pattern/Pattern51.java
@@ -0,0 +1,28 @@
+/*
+
+* * *
+ * *
+* * *
+ * *
+* * *
+
+*/
+
+class Pattern51
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if((i%2==0 && j%2==0)||(i%2!=0 && j%2!=0))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
