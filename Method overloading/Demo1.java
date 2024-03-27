d29fb217c6a72c7a48941c5d5a160da2371bc169
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-03-27 15:37:34 +0530

    Java Method overloading

---
+++ Method overloading/Demo1.java
@@ -0,0 +1,15 @@
+class Demo1
+{
+	void m1()
+	{
+		System.out.println("no argument m1 method called");
+	}
+	void m1(int a)
+	{
+		System.out.println("int argument m1 method called");
+	}
+	void m1(int a, int b)
+	{
+		System.out.println("int int argument m1 method called");
+	}
+}
---
+++ Method overloading/MainClass1.java
@@ -0,0 +1,10 @@
+class MainClass1
+{
+	public static void main(String args[])
+	{
+		Demo1 d1= new Demo1();
+		d1.m1();//no argument m1 method called
+		d1.m1(10);//int argument m1 method called
+		d1.m1(10,20);//int int argument m1 method called	
+	}
+}
