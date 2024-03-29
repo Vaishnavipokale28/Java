e19233acfc5716f4453e5c5fa99d30fb24336365
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-03-29 15:03:45 +0530

    Array program1
    write a program to find the sum of array

---
+++ Array/Program1.java
@@ -0,0 +1,11 @@
+class Program1
+{
+	public static void main(String arg[])
+	{
+		int a[]={21,39,24,22,91,12};
+		int sum=0;
+		for(int i=0;i<a.length;i++)
+			sum+=a[i];
+		System.out.println(sum);
+	}
+}
