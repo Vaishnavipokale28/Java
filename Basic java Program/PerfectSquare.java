ac0a2e06b58d31374d20b2c33a7cc47176441617
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-03 13:33:34 +0530

    Java Basic Program
    Write  a program to check given number is perfect or note

---
+++ Basic java Program/PerfectSquare.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+class PerfectSquare
+{
+	public static void main(String arg[])
+	{
+		int n=92;
+		if(Math.sqrt(n)%1==0)	
+			System.out.println(n+" is perfect square");	
+		else
+			System.out.println(n+" is not perfect sqaure")	;
+	}
+}
