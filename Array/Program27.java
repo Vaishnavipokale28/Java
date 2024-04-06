587aed49e3e49ce7b7130750629f16ffea9d86b2
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-06 15:25:23 +0530

    Java Array Program
    input: 23, 24, 18, 16, 30, 36, 32, 22, 12, 15, 17, 37}
    
    consider black number, which is divisible by 4,.
    
    consider white number, which is divisible by 6,.
    
    consider grey number, which is both black and white.
    
    replace black number with -1,.
    
    replace white number with -2,. replace grey number with -3,.
    
    output: 23,-3,-2,-1,-2,-3,-1, 22, -3,15,17,37}

---
+++ Array/Program27.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Program27
+{
+	public static void main(String args[])
+	{
+		int a[]={23,24,18,16,30,36,22,32,12,15,17,37};
+		int b[]=new int[a.length];
+		for(int i=0;i<a.length;i++)
+		{
+		if(a[i]%4==0)
+			b[i]=-1;
+		else if(a[i]%6==0)
+			b[i]=-2;
+		else if(a[i]%4==0 && a[i]%6==0)
+			b[i]=-3;
+		else
+			b[i]=a[i];	
+		}
+		System.out.println(Arrays.toString(b));		 	
+	}
+}
