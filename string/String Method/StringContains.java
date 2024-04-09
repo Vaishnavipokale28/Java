534398b09b53bd98570910f5fa812b221216f0d9
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-09 19:34:06 +0530

    Java String Method
    String Contains() method

---
+++ string/String Method/StringContains.java
@@ -0,0 +1,10 @@
+class StringContains
+{
+	public static void main(String args[])
+	{
+		String s1="What do you know about me";
+		System.out.println(s1.contains("do you know"));
+		System.out.println(s1.contains("me"));
+		System.out.println(s1.contains("i am"));
+	}
+}
