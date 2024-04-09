7879e4966be9fd32cd1eca0e702bd824df8a3064
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-09 19:18:03 +0530

    String method
    String Length()

---
+++ string/String Method/StringLength.java
@@ -0,0 +1,10 @@
+class StringLength
+{
+	public static void main(String args[])
+	{
+		String s1="Vaishnavi";
+		String s2="Pokale";
+		System.out.println("String length is:"+s1.length());
+		System.out.println("String length is:"+s2.length());
+	}
+}
