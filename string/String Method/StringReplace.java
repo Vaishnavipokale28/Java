ccf58fa006580d689e90e1ab0a8e58ce23b053ea
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-10 19:39:03 +0530

    Java String Method
    Java String Replace()

---
+++ string/String Method/StringReplace.java
@@ -0,0 +1,12 @@
+class StringReplace
+{
+	public static void main(String args[])
+	{
+		String s1="I am From SambhajiNagar";
+		String replaceString=s1.replace('a','e');
+		System.out.println(replaceString);
+		String s2="My name is vaishnavi,She is very beautiful";
+		String replace=s2.replace("is","was");
+		System.out.println(replace);				
+	}
+}
