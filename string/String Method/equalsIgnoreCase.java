8d9f770a4c5e20af0f236f767e4cb85cb88fe8f8
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-10 19:53:05 +0530

    Java String Method
    Java String equalsIgnoreCase()

---
+++ string/String Method/equalsIgnoreCase.java
@@ -0,0 +1,13 @@
+class equalsIgnoreCase
+{
+	public static void main(String args[])
+	{
+		String s1="javapoint";
+		String s2="javapoint";
+		String s3="JAVAPOINT";
+		String s4="Python";
+		System.out.println(s1.equalsIgnoreCase(s2));
+		System.out.println(s1.equalsIgnoreCase(s3));
+		System.out.println(s1.equalsIgnoreCase(s4));				
+	}
+}
