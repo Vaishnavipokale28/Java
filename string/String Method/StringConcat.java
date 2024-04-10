4404d7599a752d7fdfcaad0b90ed8848ffc1419a
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-10 16:52:35 +0530

    Java String Method
    Java String concate()

---
+++ string/String Method/StringConcat.java
@@ -0,0 +1,17 @@
+class StringConcat
+{
+	public static void main(String args[])
+	{
+		String s1="Vaishnavi";
+		String s2="Pokale";
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+		String s4="Student";
+		String s5=s1.concat(s2).concat(s4);
+		System.out.println(s5);
+		String s6="country";
+		String s7="India is my ".concat(s6);
+		System.out.println(s7);
+				
+	}
+}
