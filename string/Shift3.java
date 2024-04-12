d650836178a1b621fe75322ae7d99a535647edee
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-12 22:27:37 +0530

    Java String Program
    Write a Program to shift first character to last from each word of string

---
+++ string/Shift3.java
@@ -0,0 +1,32 @@
+/*
+Input: Hello I am Java Developer
+Output:elloH I ma avaJ eveloperD
+
+*/
+import java.util.Arrays;
+import java.util.Scanner;
+class Shift3
+{
+	public static void main(String args[])
+	{	
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter String: ");
+		String s1=sc.nextLine();
+		System.out.println(s1);
+		StringBuilder sb= new StringBuilder();
+		String s2[]=s1.split(" ");
+		for(int i=0;i<s2.length;i++)
+		{
+			sb.append(s2[i].substring(1));
+			sb.append(s2[i].charAt(0));
+				if(i<s2.length-1)
+					sb.append(" ");
+		}	
+		System.out.println(sb);	
+					
+	}
+}
+
+
+
+
