5371d0ecf7123341a0f901d424892eeb39053aed
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-12 14:50:27 +0530

    Java String Program
    Write a program to replace vowels at first

---
+++ string/Replace2.java
@@ -0,0 +1,29 @@
+/*Input: Vaishnavi
+OutPut: aiaiVshnv
+*/
+
+import java.util.Scanner;
+class Replace2
+{
+	public static void main(String args[])
+	{	
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter String: ");
+		String s1=sc.nextLine();
+		System.out.println(s1);
+		StringBuilder sb= new StringBuilder();
+		String s2="AEIOUaeiou";
+		StringBuilder vowels= new StringBuilder();
+		for(int i=0;i<s1.length();i++)
+			if(s2.contains(String.valueOf(s1.charAt(i))))
+				sb.append(s1.charAt(i));
+			else
+				vowels.append(s1.charAt(i));
+			sb.append(vowels);
+			System.out.println(sb);	
+	}
+}
+
+
+
+
