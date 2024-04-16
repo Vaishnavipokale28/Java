74a4cbcbbf361fe546f2e8b9b7d5b0adfa7e3aa2
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-16 13:18:24 +0530

    Java Inheritance

---
+++ Inheritance/SimpleInheritance/SciFiCalculator.java
@@ -0,0 +1,8 @@
+class SciFiCalculator extends Calculator
+{
+	double square(double n){return n*n;}
+	double cube(double n){return n*n*n;}
+	double squareRoot(double n){return Math.sqrt(n);}
+	double cubeRoot(double n){return Math.cbrt(n);}
+
+}
