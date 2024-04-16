92a230b297c59b0d62cd978ae39cd2bf18390d25
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-16 14:09:19 +0530

    Java multi level inheritance

---
+++ Inheritance/Multi Level Inheritance/MainClass4.java
@@ -0,0 +1,16 @@
+class MainClass4
+{
+	public static void main(String args[])
+	{
+		Area a= new Area();
+		System.out.println(a.s1);
+		System.out.println(a.circle(10));
+		System.out.println(a.circle1(3));
+		System.out.println(a.s2);
+		System.out.println(a.rectangle(30,40));
+		System.out.println(a.rectangle1(8,6));
+		System.out.println(a.s3);
+		System.out.println(a.square(50));
+		System.out.println(a.square1(30));
+	}
+}
