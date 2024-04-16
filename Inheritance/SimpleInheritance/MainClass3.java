738432ef87c3658569240f5b3f81b32b1669aa3d
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-16 13:19:13 +0530

    Java Simple inheritance example

---
+++ Inheritance/SimpleInheritance/MainClass3.java
@@ -0,0 +1,11 @@
+class MainClass3
+{
+	public static void main(String args[])
+	{
+		SciFiCalculator c= new SciFiCalculator();
+		System.out.println(c.add(10,20));
+		System.out.println(c.sub(10,20));
+		System.out.println(c.square(10));
+		System.out.println(c.cube(10));
+	}
+}
