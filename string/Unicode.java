65cc111f47d5d6822079807e2cabd5c65a4ef42a
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-17 14:09:31 +0530

    Java String Program
    Write a Java program to get the character (Unicode code point) at the given index within the string.

---
+++ string/Unicode.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Unicode
+{
+	public static void main(String args[])
+	{	
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter String: ");
+		String s1=sc.nextLine();
+		System.out.print("Enter index number to find unicode: ");
+		int a=sc.nextInt();
+		if(a>0&&a<s1.length())
+		{
+			int unicode=s1.charAt(a);
+			System.out.println("unicode value at index "+a+" is: "+unicode);
+		}
+		else
+			System.out.println("Invalid Index!!");
+		
+						
+	}
+}
+
+
+
+
