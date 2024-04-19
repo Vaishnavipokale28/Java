0b74a5b355d040e98c19b6b91af7ac52bb2a219e
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-19 14:13:59 +0530

    Java String Program
    Write a Java program to check whether a given string starts with another string.

---
+++ string/ReplaceAll.java
@@ -0,0 +1,12 @@
+
+class ReplaceAll
+{
+	public static void main(String args[])
+	{
+		
+		String s1="The quick brown fox jumps over the lazy dog";
+		String s2=s1.replace("fox","cat");
+		System.out.println(s2);
+		
+	}
+}
