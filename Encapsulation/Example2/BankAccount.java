57ba201f41924454a69b21c3b3029dee38fbd742
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-23 14:56:55 +0530

    Encapsulation
    Write a Java program to create a class called BankAccount with private instance variables accountNumber and balance. Provide public getter and setter methods to access and modify these variables.

---
+++ Encapsulation/Example2/BankAccount.java
@@ -0,0 +1,17 @@
+class BankAccount
+{
+	private String accountNumber;
+	private double balance;
+		
+	
+	public void setAccountNumber(String accountNumber){this.accountNumber=accountNumber;}
+	public void setBalance(double balance){this.balance=balance;}
+	
+	
+	String getAccountNumber(){return accountNumber;}
+	double getBalance(){return balance;}
+	
+
+	
+	}
+
