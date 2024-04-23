43d16c2811cb2e3e1d021eba5c21d9cd732d3f35
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-23 14:18:15 +0530

    Encapsulation
    Write a Java program to create a class called Person with private instance variables name, age. and country. Provide public getter and setter methods to access and modify these variables.

---
+++ Encapsulation/Example1/Person.java
@@ -0,0 +1,19 @@
+class Person
+{
+	private String name;
+	private int age;
+	private String country;
+	
+	
+	public void setName(String name){this.name=name;}
+	public void setAge(int age){this.age=age;}
+	public void setCountry(String country){this.country=country;}
+
+	
+	String getName(){return name;}
+	int getAge(){return age;}
+	String getCountry(){return country;}
+
+	
+	}
+
