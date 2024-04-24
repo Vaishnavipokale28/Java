8d3f4cd5906160eb7612ecf4de2da7f12c6d83da
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-24 15:49:33 +0530

    Java Encapsulation
    Write a Java program to create a class called Rectangle with private instance variables length and width. Provide public getter and setter methods to access and modify these variables.

---
+++ Encapsulation/Example3/MainClass7.java
@@ -0,0 +1,20 @@
+class MainClass7
+{
+	public static void main(String args[])
+	{
+		Rectangle rectangle= new Rectangle();
+		rectangle.setLength(55.00); 
+		rectangle.setWidth(34.00);
+		System.out.println("Length: "+rectangle.getLength());
+		System.out.println("Width:"+rectangle.getWidth());
+
+		
+		Rectangle rectangle2=new Rectangle(39.8,84.9);
+		System.out.println(rectangle2);
+
+	
+					
+
+				
+	}
+}
