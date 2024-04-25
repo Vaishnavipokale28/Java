39e7d16fe109effcfec2b384bd38bde5ee485792
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-04-25 15:30:44 +0530

    Java Abstraction

---
+++ Abstraction/Example/Demo6.java
@@ -0,0 +1,14 @@
+abstract class Demo6
+{
+	int a,b;
+	void m1()
+	{
+		System.out.println("m1 method of Dem06 ");
+	}
+	abstract void m2();
+
+	Demo6()
+	{
+		System.out.println("default cont of Demo6 called");
+	}
+}
