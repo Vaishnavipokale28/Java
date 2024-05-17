67997f54cb81e3d4e4dabcef9f10483e0e04901d
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-05-17 15:27:03 +0530

    Java collection
    TreeSet compareTo() method

---
+++ Collection/TreeSet/compareTo Method/student.java
@@ -0,0 +1,10 @@
+package com.model;
+
+public class student {
+	public static void main(String args[])
+	{
+		System.out.println("a".compareTo("a"));
+		System.out.println("ABC".compareTo("ACB"));
+		System.out.println("ABCiutijyf".compareTo("ABCiuti"));
+	}
+}
