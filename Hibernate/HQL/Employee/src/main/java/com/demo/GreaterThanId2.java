50bc46f1dd1362a8549708b5338e9d9504f3eaa7
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-06-19 11:36:26 +0530

    HQL
    Find greater than given id

---
+++ Hibernate/HQL/Employee/src/main/java/com/demo/GreaterThanId2.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+import java.util.List;
+import java.util.Random;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.config.HBUtils;
+import com.model.Employee;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Session session = HBUtils.sf.openSession();
+       Transaction tx = session.beginTransaction();
+       
+      session.createQuery("from Employee where eid>=:a",Employee.class)
+      .setParameter("a", 4995)
+      .list()
+      .forEach(e->System.out.println(e));
+    
+       tx.commit();
+       session.close();
+       
+    }
+}
