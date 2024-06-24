bae13e87c6063da66feff7356a5591c0f761b5c2
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-06-24 10:08:05 +0530

    Pagination

---
+++ Hibernate/Pagination/src/main/java/com/demo/App.java
@@ -0,0 +1,59 @@
+package com.demo;
+
+import java.util.List;
+import java.util.Random;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.config.HBUtils;
+import com.model.Employee;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Session session = HBUtils.sf.openSession();
+       Transaction tx = session.beginTransaction();
+    
+       paginationAll(session,50);
+      
+      tx.commit();
+      session.close();
+       
+    }
+    
+    public static void paginationAll(Session session,int size)
+    {
+    	int page=0;
+    	while(true)
+    	{
+    		List<Employee> list = session.createQuery("from Employee",Employee.class)
+    				.setMaxResults(size)
+    				.setFirstResult(page*size)
+    		.list();
+    		if(!list.isEmpty())
+    		{
+    			System.out.println("page Number: "+(++page));
+    			System.out.println("-----------------------------------");
+    			for(Employee e:list)
+    				System.out.println(e);
+    		}
+    		else
+    			break;
+    	}
+    	
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
