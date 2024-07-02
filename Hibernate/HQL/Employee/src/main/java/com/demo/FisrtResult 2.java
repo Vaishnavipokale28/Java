b41031c188b591c2f8d6c21d0b7faff22de17298
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-02 16:56:47 +0530

    First Result

---
+++ Hibernate/HQL/Employee/src/main/java/com/demo/FisrtResult 2.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import java.util.List;
+import java.util.Random;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.config.HBUtils;
+import com.model.Employee;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Session session = HBUtils.sf.openSession();
+       Transaction tx = session.beginTransaction();
+    
+       List<Employee> list = session.createQuery("from Employee",Employee.class)
+       .setFirstResult(30)
+       .setMaxResults(10)
+       .list();
+       for(Employee e:list)
+    	   System.out.println(e);
+      
+      tx.commit();
+      session.close();
+       
+    }
+}
