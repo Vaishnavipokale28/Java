c7c1a69d9fc11325a9b5c86c4165992854ae0c53
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-03 15:37:55 +0530

    HCQL

---
+++ Hibernate/HCQL/src/main/java/com/model/Employee.java
@@ -0,0 +1,56 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int eid;
+	private String ename;
+	private String edesignation;
+	private String ecompany;
+	@Column(columnDefinition = "double(10,2)")
+	private double esalary;
+		
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", edesignation=" + edesignation + ", ecompany=" + ecompany
+				+ ", esalary=" + esalary + "]";
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getEdesignation() {
+		return edesignation;
+	}
+	public void setEdesignation(String edesignation) {
+		this.edesignation = edesignation;
+	}
+	public String getEcompany() {
+		return ecompany;
+	}
+	public void setEcompany(String ecompany) {
+		this.ecompany = ecompany;
+	}
+	public double getEsalary() {
+		return esalary;
+	}
+	public void setEsalary(double esalary) {
+		this.esalary = esalary;
+	}
+
+}
