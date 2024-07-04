050e15a28f1aa93be1ea5f272919be1bf3a0e87f
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-04 17:05:26 +0530

    find employee by eid or ename or ecompany

---
+++ Hibernate/HCQL/src/main/java/com/demo/find employee by eid or ename or ecompany.java
@@ -0,0 +1,46 @@
+package com.demo;
+
+import java.util.List;
+import java.util.Random;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import com.config.HBUtils;
+import com.model.Employee;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Session session = HBUtils.sf.openSession();
+       Transaction tx = session.beginTransaction();
+    
+       
+       List<Employee> list= session.createCriteria(Employee.class)
+    		   .add(Restrictions.or(Restrictions.eq("eid",1928)).add(Restrictions.eq("ename", "YZJANVCPGCQLAPY"))
+    				   .add(Restrictions.eq("ecompany", "FXZPFHUQFLMGEJM")))
+    		   .list();
+       for(Employee e:list)
+    	   System.out.println(e);
+      
+       
+      tx.commit();
+      session.close();
+       
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
