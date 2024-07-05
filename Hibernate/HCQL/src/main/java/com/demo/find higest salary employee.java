f0af4a82033fc7c932dac7c3affd0a60b75f047c
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-05 13:19:55 +0530

    find higest salary employee

---
+++ Hibernate/HCQL/src/main/java/com/demo/find higest salary employee.java
@@ -0,0 +1,40 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import com.config.HBUtils;
+import com.model.Employee;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Session session = HBUtils.sf.openSession();
+       Transaction tx = session.beginTransaction();
+    
+       Employee e = (Employee) session.createCriteria(Employee.class)
+       .add(Restrictions.eq("esalary",session.createCriteria(Employee.class)
+       .setProjection(Projections.max("esalary"))
+       .uniqueResult()))
+       .uniqueResult();
+       System.out.println(e);
+       
+      tx.commit();
+      session.close();
+       
+    }
+   
+}
+
+
+
+
