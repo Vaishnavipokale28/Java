bef2ebdbee6a7148a1bf4e409afa49c9b70412cb
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-08 23:07:39 +0530

    find employee by salary between

---
+++ Hibernate/HCQL/src/main/java/com/demo/find employee by salary between.java
@@ -0,0 +1,38 @@
+package com.demo;
+
+import java.util.List;
+import java.util.Random;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import com.config.HBUtils;
+import com.model.Employee;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Session session = HBUtils.sf.openSession();
+       Transaction tx = session.beginTransaction();
+    
+       
+       List<Employee> list = session.createCriteria(Employee.class)
+    		   .add(Restrictions.between("esalary", 900.0, 1000.0))
+    		   .list();
+       for(Employee e:list)
+    	   System.out.println(e);
+      
+       
+      tx.commit();
+      session.close();
+       
+    }
+   
+}
+
+
+
+
