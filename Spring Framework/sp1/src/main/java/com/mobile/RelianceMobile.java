47cdcbe7e2d95767f636f8054471eaac8f0824b2
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-11 09:32:00 +0530

    Spring Framework

---
+++ Spring Framework/sp1/src/main/java/com/mobile/RelianceMobile.java
@@ -0,0 +1,24 @@
+package com.mobile;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sim.AirtelSim;
+import com.sim.RelianceSim;
+import com.sim.Sim;
+
+public class RelianceMobile {
+	ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+
+	private Sim r = context.getBean(Sim.class);
+
+	public RelianceMobile()
+	{
+		startMobile();
+		r.startSim();
+    }
+	private void startMobile()
+	{
+		System.out.println("Welcome to Reliance mobile");
+	}
+}
