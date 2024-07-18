19d32c5f9d27e0a57a1ba5bcc9fdf5e67faa28e0
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-18 09:26:50 +0530

    inject collection of primitive data type

---
+++ Spring Framework/sp2/src/main/java/com/demo/inject collection of primitive data type.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.config.MyConfiguration;
+import com.model.Student;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("bean7.xml");
+    	Student s1 = context.getBean(Student.class);
+    	System.out.println(s1);
+    	
+    }
+}
