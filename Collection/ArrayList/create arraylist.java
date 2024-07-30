d1f1b1a176ef7529bef9a6bd66f2b398a0ff4f66
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-07-30 18:30:35 +0530

    ArrayList
     Java program to create an array list, add some strings and print out the collection.

---
+++ Collection/ArrayList/create arraylist.java
@@ -0,0 +1,15 @@
+package com.demo;
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+      List<String>list=new ArrayList<String>();
+      list.add("Red");
+      list.add("Orange");
+      list.add("Pink");
+      list.add("Green");
+      list.add("Black");
+      System.out.println(list);
+    }
+}
---
+++ Collection/LinkedList/Exercise1.java
@@ -0,0 +1,15 @@
+package com.demo;
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	LinkedList<String>list=new LinkedList<String>();
+    	list.add("Red");
+    	list.add("Pink");
+    	list.add("Yellow");
+    	list.add("Green");
+    	list.add("white");
+    	System.out.println("The linked list: "+list);
+    }
+}
