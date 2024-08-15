6e1580710306fe20ed37afd652d3ac6776354282
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-08-15 22:13:12 +0530

    HashSet
    Java program to append the specified element to the end of a hash set

---
+++ Collection/HashSet/Exercise1.java
@@ -0,0 +1,15 @@
+package com.demo;
+import java.util.HashSet;
+
+public class App {
+    public static void main(String[] args)
+    {
+    	HashSet<String>hset= new HashSet<String>();
+    	hset.add("Pune");
+    	hset.add("Mumbai");
+    	hset.add("Nashik");
+    	hset.add("Solapur");
+    	hset.add("Nanded");
+    	System.out.println("HashSet: "+hset);
+    }
+}
