85223d219d35eac57f272da88c694088a6031e2d
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-08-22 22:01:18 +0530

    TreeSet
    Java program to create a tree set, add some colors (strings) and print out the tree set.

---
+++ Collection/TreeSet/Exercise1.java
@@ -0,0 +1,16 @@
+package com.demo;
+import java.util.*;
+import java.util.Iterator;
+
+public class App {
+    public static void main(String args[])
+    {
+    	TreeSet<String>tset= new TreeSet<String>();
+    	tset.add("Vaishnavi");
+    	tset.add("Sanvi");
+    	tset.add("Atharva");
+    	tset.add("Sakshi");
+    	tset.add("Samarth");
+    	System.out.println("TreeSet: "+tset);
+    }
+}
