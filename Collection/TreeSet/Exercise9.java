8f6bc58a132e6a6f6882cf6eb97e8253ea03e068
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2024-10-16 17:39:23 +0530

    TreeMap
    Java program to get a reverse order view of the keys contained in a given map.

---
+++ Collection/TreeSet/Exercise9.java
@@ -0,0 +1,20 @@
+package com.demo;
+import java.util.*;
+import java.util.Iterator;
+
+public class App {
+    public static void main(String args[])
+    {
+    	TreeMap<String, String> treeMap = new TreeMap<String,String>();
+    	
+    	treeMap.put("c1","Sanvi");
+    	treeMap.put("c2", "Vaishnavi");
+    	treeMap.put("c3","Atharva");
+    	treeMap.put("c4","Samarth");
+    	
+    	System.out.println("Orginal TreeMap content: " + treeMap);
+    	  System.out.println("Reverse order view of the keys: " + treeMap.descendingKeySet());
+    }	
+}
+
+
